6d159b7a8ac9b1f4b23310278525db8a02ebf52b
Juniper Mills <47700808+gblrfgr@users.noreply.github.com>
2022-05-11 13:01:12 -0500

    Added functionality and fixed (?) some typos
    i gotta get those tests dude i'm going insane

--- libjune/string.h
+++ libjune/string.h
@@ -11,6 +11,8 @@
 #include <assert.h>
 
 
+// OFF-BY-ONE PITFALL:
+// `contents_end` points to the null terminator. `space_end` points to the last allocated character.
 typedef struct {
     char* begin;
     char* contents_end;
@@ -20,8 +22,8 @@ typedef struct {
 
 lj_string_t lj_str_from_chars(const char* s) {
     assert(s != NULL);
-    const size_t req = strlen(s) + 1;
-    char* copy = memcpy(malloc(req), s, req);
+    const size_t req = strlen(s);
+    char* copy = memcpy(malloc(req + 1), s, req + 1);
     return (lj_string_t) {
         .begin = copy,
         .contents_end = (char*) ((intptr_t) copy + (intptr_t) req),
@@ -31,23 +33,40 @@ lj_string_t lj_str_from_chars(const char* s) {
 
 lj_string_t lj_str_from_array(const char* buf, size_t length) {
     assert(buf != NULL);
-    char* copy = memcpy(malloc(length + 1), s, req);
+    char* copy = memcpy(malloc(length + 1), s, length + 1);
     copy[length] = 0;
     return (lj_string_t) {
         .begin = copy,
-        .contents_end = (char*) ((intptr_t) copy + (intptr_t) length + 1);
-        .space_end = (char*) ((intptr_t) copy + (intptr_t) length + 1);
+        .contents_end = (char*) ((intptr_t) copy + (intptr_t) length);
+        .space_end = (char*) ((intptr_t) copy + (intptr_t) length);
     };
 }
 
+// OFF-BY-ONE PITFALL: `total_capacity` does not include the null terminator
 void lj_str_reserve(size_t total_capacity, lj_string_t* s) {
     assert(s->begin != NULL);
-    if (total_capacity < lj_str_capacity(*s)) {
+    if (total_capacity + 1 < lj_str_capacity(*s)) {
         size_t len = lj_str_length(*s);
-        s->begin = (char*) realloc(s->begin, total_capacity);
-        s->contents_end = (char*) ((intptr_t) s->begin + (intptr_t) len + 1);
+        s->begin = (char*) realloc(s->begin, total_capacity + 1);
+        s->contents_end = (char*) ((intptr_t) s->begin + (intptr_t) len);
         s->space_end = (char*) ((intptr_t) s->begin + (intptr_t) total_capacity);
     }
+}
+
+void lji_str_autoexpand(lj_string_t* s) {
+    assert(s->begin != NULL);
+    size_t necessary = lj_str_capacity(*s) + ((lj_str_capacity(*s) >> 1) | 1);
+    lj_str_reserve(necessary, s);
+}
+
+void lj_str_pushback(const char c, lj_string_t *s) {
+    assert(s->begin != NULL);
+    if (lj_str_excess(*s) == 0) {
+        lji_str_autoexpand(s);
+    }
+    *(s->contents_end) = c;
+    s->contents_end++;
+    *(s->contents_end) = '\0';
 }
 
 
@@ -68,7 +87,7 @@ inline size_t lj_str_excess(const lj_string_t s) {
 
 inline size_t lj_str_capacity(const lj_string_t s) {
     assert(s.begin != NULL);
-    return (size_t) (s.space_end - s.begin);
+    return (size_t) ((s.space_end - s.begin) + 1);
 }
 
 
