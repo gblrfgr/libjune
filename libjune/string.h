ab6036e4713e68e00ec4c57929f6c0732ed81c1f
Juniper Mills <gblrfgr@gmail.com>
2022-05-10 22:31:34 -0500

    Checked my hubris in string.h
    the sso version just. didn't work. i admitted defeat and wrote a basic bitch string implementation. for now. soon it shall be sso'd again. bc right now this is kind of just equivalent to LJ_VECTOR(char).

--- libjune/string.h
+++ libjune/string.h
@@ -4,65 +4,35 @@
 #define LIBJUNE_STRING_H
 
 
-#include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
-#include <stdint.h>
+#include <stdlib.h>
 
 typedef struct {
-  char* contents;
-  size_t length;
-  size_t capacity;
-} lji_bigstring_t;
-
-typedef union {
-  char _small[sizeof(lji_bigstring_t)];
-  lji_bigstring_t _big;
+    char* begin;
+    char* contents_end;
+    char* space_end;
 } lj_string_t;
 
-lj_string_t lj_str_from_chars(char* str) {
-  size_t req = strlen(str) + 1;
-  if (req > sizeof(lji_bigstring_t)) {
-    char* buf = (char*) malloc(req + (req >> 1));
-    memcpy(buf, str, req);
+lj_string_t lj_str_from_chars(const char* s) {
+    const size_t req = strlen(s) + 1;
+    char* buf = memcpy(malloc(req), s, req);
     return (lj_string_t) {
-      ._big = (lji_bigstring_t) {
-        .contents = buf,
-        .capacity = (req + (req >> 1) - 1) | 1,
-        .length = req - 1,
-      },
+        .begin = buf,
+        .contents_end = (char*) ((intptr_t) buf + (intptr_t) req),
+        .space_end = (char*) ((intptr_t) buf + (intptr_t) req),
     };
-  } else {
-    lj_string_t res = (lj_string_t) {0};
-    memcpy(res._small, str, req);
-    res._small[sizeof(lj_string_t) - 1] = (24 - req) << 2;
-    return res;
-  }
 }
 
-bool lj_str_is_big(lj_string_t str) {
-  return (bool) ((intptr_t) str._big.capacity & 1);
+const char* lj_str_cstr(const lj_string_t s) {
+    return s.begin;
 }
 
-char* lj_str_cstr(lj_string_t *s) {
-  if (lj_str_is_big(*s)) {
-    return s->_big.contents;
-  } else {
-    return s->_small;
-  }
+size_t lj_str_length(const lj_string_t s) {
+    return (size_t) (s.contents_end - s.begin);
 }
 
-size_t lj_str_length(lj_string_t *s) {
-  if (lj_str_is_big(*s)) {
-    return s->_big.length;
-  } else {
-    return (size_t) 23 - (s->_small[sizeof(lj_string_t) - 1] >> 1);
-  }
-}
-
-void lji_bigstr_expand(lji_bigstring_t* b) {
-  b->capacity = (b->capacity + (b->capacity >> 1)) | 1;
-  b->contents = (char*) realloc(b->contents, b->capacity);
+size_t lj_str_excess(const lj_string_t s) {
+    return (size_t) (s.space_end - s.contents_end);
 }
 
 
