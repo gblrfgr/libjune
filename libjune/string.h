7acf51d226729db6aa7a83b1b60dce066c0182d2
Juniper Mills <gblrfgr@gmail.com>
2022-05-10 23:20:54 -0500

    Updated strings
    documentation for strings isn't done. i am tired. sad!

--- libjune/string.h
+++ libjune/string.h
@@ -4,8 +4,11 @@
 #define LIBJUNE_STRING_H
 
 
+#define LJI_DEAD_STRING_SENTINEL_K  0xDEADC0DE
+
 #include <string.h>
 #include <stdlib.h>
+#include <assert.h>
 
 typedef struct {
     char* begin;
@@ -14,26 +17,53 @@ typedef struct {
 } lj_string_t;
 
 lj_string_t lj_str_from_chars(const char* s) {
+    assert(s != NULL);
     const size_t req = strlen(s) + 1;
-    char* buf = memcpy(malloc(req), s, req);
+    char* copy = memcpy(malloc(req), s, req);
     return (lj_string_t) {
-        .begin = buf,
-        .contents_end = (char*) ((intptr_t) buf + (intptr_t) req),
-        .space_end = (char*) ((intptr_t) buf + (intptr_t) req),
+        .begin = copy,
+        .contents_end = (char*) ((intptr_t) copy + (intptr_t) req),
+        .space_end = (char*) ((intptr_t) copy + (intptr_t) req),
     };
 }
 
-const char* lj_str_cstr(const lj_string_t s) {
+lj_string_t lj_str_from_array(const char* buf, size_t length) {
+    assert(buf != NULL);
+    char* copy = memcpy(malloc(length + 1), s, req);
+    copy[length] = 0;
+    return (lj_string_t) {
+        .begin = copy,
+        .contents_end = (char*) ((intptr_t) copy + (intptr_t) length + 1);
+        .space_end = (char*) ((intptr_t) copy + (intptr_t) length + 1);
+    };
+}
+
+inline const char* lj_str_cstr(const lj_string_t s) {
+    assert(s.begin != NULL);
     return s.begin;
 }
 
-size_t lj_str_length(const lj_string_t s) {
+inline size_t lj_str_length(const lj_string_t s) {
+    assert(s.begin != NULL);
     return (size_t) (s.contents_end - s.begin);
 }
 
-size_t lj_str_excess(const lj_string_t s) {
+inline size_t lj_str_excess(const lj_string_t s) {
+    assert(s.begin != NULL);
     return (size_t) (s.space_end - s.contents_end);
 }
+
+void lj_str_destroy(lj_string_t* s) {
+    assert(s.begin != NULL);
+    free(s->begin);
+    
+    // salt the earth to avoid undefined behavior
+    s->begin = (char*) NULL;
+    s->contents_end = (char*) LJI_DEAD_STRING_SENTINEL_K;
+    s->space_end = (char*) LJI_DEAD_STRING_SENTINEL_K;
+}
+
+#undef  LJI_DEAD_STRING_SENTINEL_K
 
 
 #endif
