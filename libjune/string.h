559e7961729853953583c69afb11d168af4ba37e
Juniper Mills <gblrfgr@gmail.com>
2022-05-14 16:39:06 -0500

    Unf#ckiwuckified string.h
    seriously why were you trying to be libc++ you aren't libc++

--- libjune/string.h
+++ libjune/string.h
@@ -1,5 +1,3 @@
-/*  Documentation found in docs/string.md  */
-
 #ifndef LIBJUNE_STRING_H
 #define LIBJUNE_STRING_H
 
@@ -10,40 +8,27 @@
 #include <assert.h>
 #include <stdbool.h>
 
-#define LJI_DEAD_STRING_SENTINEL_K  0xDEADC0DE
-
-// OFF-BY-ONE PITFALL:
-// `contents_end` points to the null terminator. `space_end` points to the last allocated character.
+// length and capacity do not include the null terminator
 typedef struct {
     char* begin;
-    char* contents_end;
-    char* space_end;
+    size_t length;
+    size_t capacity;
 } lj_string_t;
 
+typedef struct {
+    const char* const begin;
+    const size_t length;
+} lj_stringview_t;
 
-inline size_t lj_str_length(const lj_string_t s) {
-    assert(s.begin != NULL);
-    return (size_t) (s.contents_end - s.begin);
-}
-
-inline size_t lj_str_excess(const lj_string_t s) {
-    assert(s.begin != NULL);
-    return (size_t) (s.space_end - s.contents_end);
-}
-
-inline size_t lj_str_capacity(const lj_string_t s) {
-    assert(s.begin != NULL);
-    return (size_t) ((s.space_end - s.begin) + 1);
-}
 
 lj_string_t lj_str_from_chars(const char* s) {
     assert(s != NULL);
-    const size_t req = strlen(s);
-    char* copy = memcpy(malloc(req + 1), s, req + 1);
+    const size_t req = strlen(s) + 1;
+    char* copy = memcpy(malloc(req), s, req);
     return (lj_string_t) {
         .begin = copy,
-        .contents_end = (char*) ((intptr_t) copy + (intptr_t) req),
-        .space_end = (char*) ((intptr_t) copy + (intptr_t) req),
+        .length = req - 1,
+        .capacity = req - 1,
     };
 }
 
@@ -52,8 +37,8 @@ lj_string_t lj_str_new_empty() {
     *space = '\0';
     return (lj_string_t) {
         .begin = space,
-        .contents_end = space,
-        .space_end = space,
+        .length = 0,
+        .capacity = 0,
     };
 }
 
@@ -63,19 +48,17 @@ lj_string_t lj_str_from_array(const char* buf, size_t length) {
     copy[length] = 0;
     return (lj_string_t) {
         .begin = copy,
-        .contents_end = (char*) ((intptr_t) copy + (intptr_t) length),
-        .space_end = (char*) ((intptr_t) copy + (intptr_t) length),
+        .length = length,
+        .capacity = length,
     };
 }
 
-// OFF-BY-ONE PITFALL: `total_capacity` does not include the null terminator
-void lj_str_reserve(size_t total_capacity, lj_string_t* s) {
+// total_capacity does not include the null terminator
+void lj_str_reserve(lj_string_t* s, size_t total_capacity) {
     assert(s->begin != NULL);
-    if (total_capacity + 1 > lj_str_capacity(*s)) {
-        size_t len = lj_str_length(*s);
+    if (total_capacity > s->capacity) {
         s->begin = (char*) realloc(s->begin, total_capacity + 1);
-        s->contents_end = (char*) ((intptr_t) s->begin + (intptr_t) len);
-        s->space_end = (char*) ((intptr_t) s->begin + (intptr_t) total_capacity);
+        s->capacity = total_capacity;
     }
 }
 
@@ -85,29 +68,22 @@ void lji_str_autoexpand(lj_string_t* s) {
     lj_str_reserve(necessary, s);
 }
 
-void lj_str_pushback(const char c, lj_string_t *s) {
+void lj_str_pushback( lj_string_t *s, const char c) {
     assert(s->begin != NULL);
-    if (lj_str_excess(*s) == 0) {
+    if (s->length == s->capacity) {
         lji_str_autoexpand(s);
     }
-    *(s->contents_end) = c;
-    s->contents_end++;
-    *(s->contents_end) = '\0';
+    (s->begin)[s->length] = c;
+    s->length++;
+    (s->begin)[s->length] = '\0';
 }
 
-void lj_str_pushstr(const char* s, lj_string_t *onto) {
+void lj_str_pushstr(lj_string_t *onto, const char* s) {
     assert(onto->begin != NULL);
     size_t len = strlen(s);
-    while (len > lj_str_excess(*onto)) {
-        lji_str_autoexpand(onto);
-    }
-    memcpy(onto->contents_end, s, len + 1);
-    onto->contents_end += len;
-}
-
-inline const char* lj_str_cstr(const lj_string_t s) {
-    assert(s.begin != NULL);
-    return s.begin;
+    lj_str_reserve(onto, len + s->length);
+    memcpy(onto->begin + onto->length, s, len + 1);
+    onto->length += len;
 }
 
 
@@ -115,10 +91,10 @@ void lj_str_destroy(lj_string_t* s) {
     assert(s->begin != NULL);
     free(s->begin);
     
-    // salt the earth to avoid undefined behavior
+    // salt the earth
     s->begin = (char*) NULL;
-    s->contents_end = (char*) LJI_DEAD_STRING_SENTINEL_K;
-    s->space_end = (char*) LJI_DEAD_STRING_SENTINEL_K;
+    s->length = ~0;
+    s->capacity = ~0;
 }
 
 
