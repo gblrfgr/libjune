055715f870044362b0776a7912059658cda11832
Juniper Mills <gblrfgr@gmail.com>
2022-05-14 18:05:48 -0500

    Propagated string.h changes
    unf#ckiwuckification complete

--- libjune/logging.h
+++ libjune/logging.h
@@ -38,7 +38,6 @@ lj_logger_t lj_build_logger(const char* format, lj_loglevel_t loglevel, FILE* ou
     };
 }
 
-// example format string: "[$M/$D/$Y $h:$m:$s $l]\t$n"
 const char* lji_logger_format(const char* format, const char* message, const struct tm* gmt, lj_loglevel_t level) {
     lj_string_t res = lj_str_new_empty();
     const char* cursor = format;
@@ -46,50 +45,50 @@ const char* lji_logger_format(const char* format, const char* message, const str
         if (*cursor == '\\') {
             cursor++;
             assert(*cursor != '\0');
-            lj_str_pushback(*cursor, &res);
+            lj_str_pushback(&res, *cursor);
         } else if (*cursor == '$') {
             cursor++;
             switch (*cursor) {
                 case 'M': 
-                    lj_str_pushback('0' + (gmt->tm_mon + 1) / 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_mon + 1) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_mon + 1) / 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_mon + 1) % 10);
                     break;
                 case 'D':
-                    lj_str_pushback('0' + (gmt->tm_mday + 1) / 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_mday + 1) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_mday + 1) / 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_mday + 1) % 10);
                     break;
                 case 'Y':
-                    lj_str_pushback('0' + (gmt->tm_year + 1901) / 1000, &res);
-                    lj_str_pushback('0' + ((gmt->tm_year + 1901) / 100) % 10, &res);
-                    lj_str_pushback('0' + ((gmt->tm_year + 1901) / 10) % 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_year + 1901) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_year + 1901) / 1000);
+                    lj_str_pushback(&res, '0' + ((gmt->tm_year + 1901) / 100) % 10);
+                    lj_str_pushback(&res, '0' + ((gmt->tm_year + 1901) / 10) % 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_year + 1901) % 10);
                     break;
                 case 'h':
-                    lj_str_pushback('0' + (gmt->tm_hour + 1) / 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_hour + 1) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_hour + 1) / 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_hour + 1) % 10);
                     break;
                 case 'm':
-                    lj_str_pushback('0' + (gmt->tm_min + 1) / 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_min + 1) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_min + 1) / 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_min + 1) % 10);
                     break;
                 case 's':
-                    lj_str_pushback('0' + (gmt->tm_sec + 1) / 10, &res);
-                    lj_str_pushback('0' + (gmt->tm_sec + 1) % 10, &res);
+                    lj_str_pushback(&res, '0' + (gmt->tm_sec + 1) / 10);
+                    lj_str_pushback(&res, '0' + (gmt->tm_sec + 1) % 10);
                     break;
                 case 'l':
-                    lj_str_pushstr(LJI_LOGLEVEL_NAMES_K[level], &res);
+                    lj_str_pushstr(&res, LJI_LOGLEVEL_NAMES_K[level]);
                     break;
                 case 'n':
-                    lj_str_pushstr(message, &res);
+                    lj_str_pushstr(&res, message);
                     break;
                 case '$':
-                    lj_str_pushback('$', &res);
+                    lj_str_pushback(&res, '$');
                     break;
                 default:
                     assert(false);
             }
         } else {
-            lj_str_pushback(*cursor, &res);
+            lj_str_pushback(&res, *cursor);
         }
         cursor++;
     }
