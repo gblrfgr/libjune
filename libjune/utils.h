0bcc511373d49bce348ad1180645724fd0ffdbac
Juniper Mills <gblrfgr@gmail.com>
2022-05-14 16:37:27 -0500

    Added lj_isalnum to utils.h
    does this copy a libc function? i think it does. rip.

--- libjune/utils.h
+++ libjune/utils.h
@@ -4,9 +4,17 @@
 #define LIBJUNE_UTILS_H
 
 
+#include <stdbool.h>
+
 #define   LJ_SWAP(type, a, b)  do { type temp = *(a); \
                                     *(a) = *(b); \
                                     *(b) = temp; } while (0);
 
+
+inline bool lj_isalnum(char c) {
+    return ('a' <= c && c <= 'z') ||
+           ('A' <= c && c <= 'Z') ||
+           ('0' <= c && c <= '9');
+}
 
 #endif
