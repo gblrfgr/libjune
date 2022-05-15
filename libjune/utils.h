1a00a990b7b2840e002bb96f990447c03d19bb0d
Juniper Mills <gblrfgr@gmail.com>
2022-05-14 21:22:24 -0500

    Update utils.h
    minor formatting change

--- libjune/utils.h
+++ libjune/utils.h
@@ -6,8 +6,8 @@
 
 #include <stdbool.h>
 
-#define   LJ_SWAP(type, a, b)  do { type temp = *(a); \
-                                    *(a) = *(b); \
+#define   LJ_SWAP(type, a, b)  do { type temp = *(a);           \
+                                    *(a) = *(b);                \
                                     *(b) = temp; } while (0);
 
 
